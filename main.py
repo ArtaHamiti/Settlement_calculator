a63489588c6fc7b2b71f04b8b8c008662b1e5cdb
gustavbagger <gustavbagger@me.com>
2026-09-12 02:22:46 +1000

    find, remove and add payments

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
-print("Velkommen. Velg handling: [1/2]")
-print("1. Se status for hvem som skylder hvem.")
-print("2. Legg inn et utlegg")
+
+import db
+
+def main():
+    print("Velkommen. Velg handling: [1/2]")
+    print("1. Se status for hvem som skylder hvem.")
+    print("2. Legg inn et utlegg")
+
+    dbPath = "data.csv"
+
+    db.MakeCSV(dbPath)
+    
+    db.AddPayment(dbPath,"Gustav",10.0)
+
+    db.FindPayments(dbPath)
+
+    return
+
+main()
